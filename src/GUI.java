4260c6ea44107313c3646aea7b74365ed9ba12f3
Jose Llamas <58204861+jllamas626@users.noreply.github.com>
2020-12-09 20:24:01 -0500

    GUI commit

--- src/GUI.java
+++ src/GUI.java
@@ -4,11 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
-    private JButton[] buttons = new JButton[7];
-    private JFrame frame = new JFrame();
-    private String message = "Test Question";
-    private JLabel label = new JLabel("What is your favorite pokemon", JLabel.CENTER);
-    private JPanel panel = new JPanel();
+    private static JButton[] buttons = new JButton[5];
+    private static JFrame frame = new JFrame();
+    private static String message = "Begin!";
+    private static JLabel label = new JLabel("What is your favorite pokemon", JLabel.CENTER);
+    private static JPanel panel = new JPanel();
+    int count = 0;
 
     public GUI() {
 
@@ -19,6 +20,9 @@ public class GUI implements ActionListener {
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new JButton(message);
             buttons[i].addActionListener(this);
+            if (i != 0) {
+                buttons[i].setVisible(false);
+            }
             buttons[i].setName(Integer.toString(i));
             panel.add(buttons[i]);
         }
@@ -33,14 +37,121 @@ public class GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == buttons[0]) {
-            System.out.println("Test");
+            Game.answers.add(1);
+            System.out.println(Game.answers);
+            count++;
         } else if (e.getSource() == buttons[1]) {
-            System.out.println("Test2");
+            Game.answers.add(2);
+            System.out.println(Game.answers);
+            count++;
         } else if (e.getSource() == buttons[2]) {
-
+            Game.answers.add(3);
+            System.out.println(Game.answers);
+            count++;
         } else if (e.getSource() == buttons[3]) {
-
+            Game.answers.add(4);
+            System.out.println(Game.answers);
+            count++;
         } else if (e.getSource() == buttons[4]) {
+            Game.answers.add(5);
+            System.out.println(Game.answers);
+            count++;
+        }
+        setUpQuestion(count);
+    }
+
+    public static void setUpQuestion(int numQuestion) {
+        for (JButton btn : buttons) {
+            btn.setVisible(true);
+        }
+        switch (numQuestion) {
+            case 0:
+                label.setText("Do you have a short fuse?");
+                buttons[0].setText("Strongly Agree");
+                buttons[1].setText("Agree");
+                buttons[2].setText("Neutral");
+                buttons[3].setText("Disagree");
+                buttons[4].setText("Strongly Disagree");
+                break;
+            case 1:
+                label.setText("Do you get along with most people?");
+                buttons[0].setText("Strongly Agree");
+                buttons[1].setText("Agree");
+                buttons[2].setText("Neutral");
+                buttons[3].setText("Disagree");
+                buttons[4].setText("Strongly Disagree");
+                break;
+            case 2:
+                label.setText("what is your favorite season?");
+                buttons[0].setText("Spring");
+                buttons[1].setText("Summer");
+                buttons[2].setText("Autumn");
+                buttons[3].setText("Winter");
+                buttons[4].setText("Who cares about seasons?");
+                break;
+            case 3:
+                label.setText("Which of these are your preferred sport?");
+                buttons[0].setText("Basketball");
+                buttons[1].setText("Soccer");
+                buttons[2].setText("NASCAR");
+                buttons[3].setText("Speed Boat Racing");
+                buttons[4].setText("Bowling(lol)");
+                break;
+            case 4:
+                label.setText("How do you spend your spare time?");
+                buttons[0].setText("Reading");
+                buttons[1].setText("Playing video games");
+                buttons[2].setText("Hanging out with friends");
+                buttons[3].setText("Exercising");
+                buttons[4].setText("Watching TV");
+                break;
+            case 5:
+                label.setText("Which fictional land would you live in?");
+                buttons[0].setText("Bikini Bottom (Spongebob)");
+                buttons[1].setText("Skyrim");
+                buttons[2].setText("Gotham");
+                buttons[3].setText("Kanto (Pokemon)");
+                buttons[4].setText("Springfield (Simpsons)");
+                break;
+            case 6:
+                label.setText("Which of these items do order at a restaurant?");
+                buttons[0].setText("Hamburger");
+                buttons[1].setText("Pizza");
+                buttons[2].setText("Tacos");
+                buttons[3].setText("Shrimp Lo Mein");
+                buttons[4].setText("Sushi");
+                break;
+            case 7:
+                label.setText("Your superpower of choice?");
+                buttons[0].setText("Water Mastery");
+                buttons[1].setText("Fire Mastery");
+                buttons[2].setText("Earth Mastery");
+                buttons[3].setText("Electricity Mastery");
+                buttons[4].setText("Psychic Abilities");
+                break;
+            case 8:
+                label.setText("Preferred Marvel villain?");
+                buttons[0].setText("Thanos(Avengers)");
+                buttons[1].setText("Loki(Thor)");
+                buttons[2].setText("The Green Goblin(Spider-man)");
+                buttons[3].setText("The Red Skull(Captain America)");
+                buttons[4].setText("Magneto (X-Men)");
+                break;
+            case 9:
+                label.setText("You need to break into a locked box, what method do you use?");
+                buttons[0].setText("Pick the lock");
+                buttons[1].setText("Blow that sucker open with a grenade");
+                buttons[2].setText("Find the key");
+                buttons[3].setText("Saw around the lock");
+                buttons[4].setText("Walk away, the box is locked for a reason");
+                break;
+            default:
+                for (JButton btn : buttons) {
+                    btn.setVisible(false);
+                }
+                Game newGame = new Game();
+                int result = Questions.calculate(Game.answers);
+                label.setText("Congratulations! your spirit pokemon is..." + newGame.pokeMap.get(result) + "!");
 
         }
     }
--- src/Questions.java
+++ src/Questions.java
@@ -26,7 +26,7 @@ public class Questions {
         }
     }
 
-    public static String calculate(ArrayList<Integer> list, String name, HashMap<Integer, String> pokeMap) {
+    public static int calculate(ArrayList<Integer> list) {
         //Integer one = 0, two = 0, three = 0, four = 0, five = 0;
         HashMap<Integer, Integer> calcMap = new HashMap<Integer, Integer>();
 
@@ -66,20 +66,23 @@ public class Questions {
                 largest = i;
             }
         }
-
         System.out.println(largest);
-        switch(largest) {
-            case 1:
-                return "congratulations " + name + " your spirit Pokemon is " + pokeMap.get(1) + "!";
-            case 2:
-                return "congratulations " + name + " your spirit Pokemon is " + pokeMap.get(2) + "!";
-            case 3:
-                return "congratulations " + name + " your spirit Pokemon is " + pokeMap.get(3) + "!";
-            case 4:
-                return "congratulations " + name + " your spirit Pokemon is " + pokeMap.get(4) + "!";
-            case 5:
-                return "congratulations " + name + " your spirit Pokemon is " + pokeMap.get(5) + "!";
-        }
-        return "";
+        return largest;
+
+//
+//        switch(largest) {
+//            case 1:
+//                return 1;
+//            case 2:
+//                return 2;
+//            case 3:
+//                return 3;
+//            case 4:
+//                return 4;
+//            case 5:
+//                return 5;
+//        }
+//        return 0;
     }
+
 }
